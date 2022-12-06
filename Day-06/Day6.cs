67d04543aebec88edb4ead994010ba5fc1cc6e13
Aart <aart_odding@hotmail.com>
2022-12-06 20:25:15 +0100

    Day 6

---
+++ Day-06/Day6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace aoc
+{
+    static class Day6
+    {
+        public static bool AllUnique(char[] input)
+        {
+            return input.ToHashSet().Count == input.Length;
+        }
+
+        public static void Part1()
+        {
+            string input = Input.AsString(6);
+
+            char[] buffer = new char[4];
+            int bufferPos = 0;
+
+            for (int index = 0; index < input.Length; ++index)
+            {
+                buffer[bufferPos] = input[index];
+                bufferPos = (bufferPos + 1) % buffer.Length;
+
+                if (AllUnique(buffer) && index >= (buffer.Length - 1))
+                {
+                    Console.WriteLine(index + 1);
+                    break;
+                }
+            }
+        }
+
+        public static void Part2()
+        {
+            string input = Input.AsString(6);
+
+            char[] buffer = new char[14];
+            int bufferPos = 0;
+
+            for (int index = 0; index < input.Length; ++index)
+            {
+                buffer[bufferPos] = input[index];
+                bufferPos = (bufferPos + 1) % buffer.Length;
+
+                if (AllUnique(buffer) && index >= (buffer.Length - 1))
+                {
+                    Console.WriteLine(index + 1);
+                    break;
+                }
+            }
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -25,9 +25,11 @@ namespace aoc
             //Day4.Part1();
             //Day4.Part2();
 
-            Day5.Part1();
-            Day5.Part2();
+            //Day5.Part1();
+            //Day5.Part2();
 
+            Day6.Part1();
+            Day6.Part2();
         }
     }
 }
